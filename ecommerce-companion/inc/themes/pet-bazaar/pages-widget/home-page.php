880bd7c7c2320d281a9bca341d516ddade72e23d
Sean Murphy <sean@iamseanmurphy.com>
2025-02-25 14:18:32 +0000

    8.1.35

--- ecommerce-companion/inc/themes/pet-bazaar/pages-widget/home-page.php
+++ ecommerce-companion/inc/themes/pet-bazaar/pages-widget/home-page.php
@@ -25,4 +25,23 @@
 		update_option( 'page_on_front', $page->ID );
 		
 	}
+	
+/* Set Default Values */
+set_theme_mod('abv_hdr_antext', __('Save up 35% off today, Supper Value Deals, Welcome To Our Pet Bazaar!'));
+set_theme_mod('header_button_label', __('Find Store'));
+set_theme_mod('header_button_link', '#');
+set_theme_mod('hdr_contact_ttl', __('Contact Us Free'));
+set_theme_mod('hdr_contact_subttl', __('+1234567890'));
+set_theme_mod('visa_icon', 'fa-cc-visa');
+set_theme_mod('visa_link', '#');
+set_theme_mod('paypal_icon', 'fa-cc-paypal');
+set_theme_mod('paypal_link', '#');
+set_theme_mod('mastercard_icon', 'fa-cc-mastercard');
+set_theme_mod('mastercard_link', '#');
+set_theme_mod('amex_icon', 'fa-cc-amex');
+set_theme_mod('amex_link', '#');
+set_theme_mod('discover_icon', 'fa-cc-discover');
+set_theme_mod('discover_link', '#');
+set_theme_mod('jcb_icon', 'fa-cc-jcb');
+set_theme_mod('jcb_link', '#');
 ?>
--- ecommerce-companion/inc/themes/pet-bazaar/pet-bazaar.php
+++ ecommerce-companion/inc/themes/pet-bazaar/pet-bazaar.php
@@ -24,21 +24,3 @@ if ( ! function_exists( 'ecommerce_comp_pet_bazaar_frontpage_sections' ) ) :
     }
 	add_action( 'pet_bazaar_sections', 'ecommerce_comp_pet_bazaar_frontpage_sections' );
 endif;
-
-set_theme_mod('abv_hdr_antext', __('Save up 35% off today, Supper Value Deals, Welcome To Our Pet Bazaar!'));
-set_theme_mod('header_button_label', __('Find Store'));
-set_theme_mod('header_button_link', '#');
-set_theme_mod('hdr_contact_ttl', __('Contact Us Free'));
-set_theme_mod('hdr_contact_subttl', __('+1234567890'));
-set_theme_mod('visa_icon', 'fa-cc-visa');
-set_theme_mod('visa_link', '#');
-set_theme_mod('paypal_icon', 'fa-cc-paypal');
-set_theme_mod('paypal_link', '#');
-set_theme_mod('mastercard_icon', 'fa-cc-mastercard');
-set_theme_mod('mastercard_link', '#');
-set_theme_mod('amex_icon', 'fa-cc-amex');
-set_theme_mod('amex_link', '#');
-set_theme_mod('discover_icon', 'fa-cc-discover');
-set_theme_mod('discover_link', '#');
-set_theme_mod('jcb_icon', 'fa-cc-jcb');
-set_theme_mod('jcb_link', '#');
